1955c4557be62e36d6d09b449fea4337c11be0b5
Raymond Dodge <git@rayrobdod.name>
2015-03-30 10:23:36 -0400

    Creating testcases which test parts of SeqBuilder and MapBuilder that could not possibly have failed.

---
+++ src/test/scala/com/rayrobdod/json/builder/MapBuilderTest.scala
@@ -0,0 +1,99 @@
+/*
+	Copyright (c) 2015, Raymond Dodge
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+		* Redistributions of source code must retain the above copyright
+		  notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright
+		  notice, this list of conditions and the following disclaimer in the
+		  documentation and/or other materials provided with the distribution.
+		* Neither the name "<PRODUCT NAME>" nor the names of its contributors
+		  may be used to endorse or promote products derived from this software
+		  without specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONMapUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package com.rayrobdod.json.builder;
+
+import scala.reflect.BeanProperty;
+import java.text.ParseException;
+import scala.collection.immutable.Map;
+import org.scalatest.FunSpec;
+
+class MapBuilderTest extends FunSpec {
+	
+	describe("MapBuilder") {
+		it ("inits correctly") {
+			assertResult(Map.empty){new MapBuilder().init}
+		}
+		it ("Appends value") {
+			val myValue = new Object
+			
+			assertResult(Map("sdfa" -> myValue)){
+				new MapBuilder().apply(Map.empty, "sdfa", myValue)
+			}
+		}
+		it ("Appends value 2") {
+			val myValue1 = new Object
+			val myValue2 = new Object
+			
+			assertResult(Map("a" -> myValue1, "b" -> myValue2)){
+				new MapBuilder().apply(Map("a" -> myValue1), "b", myValue2)
+			}
+		}
+		it ("childBuilder returns value from constructor") {
+			import BeanBuilderTest.MockBuilder
+			val key = "sdafdsfa"
+			
+			assertResult(MockBuilder){
+				new MapBuilder({s => if (s == key) {MockBuilder} else {throw new IllegalArgumentException(s)}}).childBuilder(key)
+			}
+		}
+		it ("childBuilder returns default value if no constructor") {
+			assert{
+				new MapBuilder().childBuilder("sdafdsfa").isInstanceOf[MapBuilder]
+			}
+		}
+		it ("resultType returns constructor parameter `clazz`") {
+			assertResult(classOf[Map[_,_]]){
+				new MapBuilder().resultType
+			}
+		}
+	}
+	
+	describe("MapBuilder integration") {
+		import com.rayrobdod.json.parser.JsonParser
+		import BeanBuilderTest.Person
+		
+		it ("MapBuilder + JsonParser + primitive") {
+			assertResult(Map("a" -> 61, "b" -> 62, "c" -> 63)){
+				new JsonParser(new MapBuilder).parse(
+					"""{"a":61, "b":62, "c":63}"""
+				)
+			}
+		}
+		it ("MapBuilder + JsonParser + BeanBuilder") {
+			assertResult(Map("red" -> Person("Mario", 32),"green" -> Person("Luigi", 32),"pink" -> Person("Peach", 28))){
+				new JsonParser(new MapBuilder({s => new BeanBuilder(classOf[Person])})).parse(
+					"""{
+						"red":{"name":"Mario", "age":32},
+						"green":{"name":"Luigi", "age":32},
+						"pink":{"name":"Peach", "age":28}
+					}"""
+				)
+			}
+		}
+	}
+}
+
---
+++ src/test/scala/com/rayrobdod/json/builder/SeqBuilderTest.scala
@@ -0,0 +1,98 @@
+/*
+	Copyright (c) 2015, Raymond Dodge
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+		* Redistributions of source code must retain the above copyright
+		  notice, this list of conditions and the following disclaimer.
+		* Redistributions in binary form must reproduce the above copyright
+		  notice, this list of conditions and the following disclaimer in the
+		  documentation and/or other materials provided with the distribution.
+		* Neither the name "<PRODUCT NAME>" nor the names of its contributors
+		  may be used to endorse or promote products derived from this software
+		  without specific prior written permission.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package com.rayrobdod.json.builder;
+
+import scala.reflect.BeanProperty;
+import java.text.ParseException;
+import scala.collection.immutable.Seq;
+import org.scalatest.FunSpec;
+
+class SeqBuilderTest extends FunSpec {
+	
+	describe("SeqBuilder") {
+		it ("inits correctly") {
+			assertResult(Nil){new SeqBuilder().init}
+		}
+		it ("Appends value") {
+			val myValue = new Object
+			
+			assertResult(Seq(myValue)){
+				new SeqBuilder().apply(Nil, "sdfa", myValue)
+			}
+		}
+		it ("Appends value 2") {
+			val myValue1 = new Object
+			val myValue2 = new Object
+			
+			assertResult(Seq(myValue1, myValue2)){
+				new SeqBuilder().apply(Seq(myValue1), "sdfa", myValue2)
+			}
+		}
+		it ("childBuilder returns value from constructor") {
+			import BeanBuilderTest.MockBuilder
+			
+			assertResult(MockBuilder){
+				new SeqBuilder(MockBuilder).childBuilder("asdf")
+			}
+		}
+		it ("childBuilder returns default value if no constructor") {
+			assert{
+				new SeqBuilder().childBuilder("sdafdsfa").isInstanceOf[SeqBuilder]
+			}
+		}
+		it ("resultType returns constructor parameter `clazz`") {
+			assertResult(classOf[Seq[_]]){
+				new SeqBuilder().resultType
+			}
+		}
+	}
+	
+	describe("SeqBuilder integration") {
+		import com.rayrobdod.json.parser.JsonParser
+		import BeanBuilderTest.Person
+		
+		it ("SeqBuilder + JsonParser + primitive") {
+			assertResult(Seq("a", "b", "c")){
+				new JsonParser(new SeqBuilder).parse(
+					"""["a", "b", "c"]"""
+				)
+			}
+		}
+		it ("SeqBuilder + JsonParser + BeanBuilder") {
+			assertResult(Seq(Person("Mario", 32),Person("Luigi", 32),Person("Peach", 28))){
+				new JsonParser(new SeqBuilder(new BeanBuilder(classOf[Person]))).parse(
+					"""[
+						{"name":"Mario", "age":32},
+						{"name":"Luigi", "age":32},
+						{"name":"Peach", "age":28},
+					]"""
+				)
+			}
+		}
+	}
+}
+
