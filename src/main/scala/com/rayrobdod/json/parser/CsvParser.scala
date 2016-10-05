b995704f919783936bb23645e0d43056d68dc498
Raymond Dodge <git@rayrobdod.name>
2016-10-05 00:56:45 -0400

    Extract LineParser from CsvParser and CsvWithHeaderParser
    So that the two may share the same class

--- src/main/scala/com/rayrobdod/json/parser/CsvParser.scala
+++ src/main/scala/com/rayrobdod/json/parser/CsvParser.scala
@@ -44,6 +44,8 @@ import com.rayrobdod.json.union.ParserRetVal
 final class CsvParser(
 		meaningfulCharacters:CsvParser.CharacterMeanings = CsvParser.csvCharacterMeanings
 ) extends Parser[Int, String, Iterable[Char]] {
+	import CsvParser.State
+	val lineParser = new CsvParser.LineParser(meaningfulCharacters)
 	
 	/**
 	 * Decodes the input values to an object.
@@ -67,7 +69,7 @@ final class CsvParser(
 				state.appendChar(char).copy(quoted = true)
 			} else if (meaningfulCharacters.recordDelimeter contains char) {
 				new State(
-					value = state.value.right.flatMap{x => builder.apply(x, state.innerIndex, state.innerInput, new LineParser).left.map{x => ((x._1, x._2 + index))}},
+					value = state.value.right.flatMap{x => builder.apply(x, state.innerIndex, state.innerInput, lineParser).left.map{x => ((x._1, x._2 + index))}},
 					innerIndex = state.innerIndex + 1,
 					innerInput = "",
 					endingWhitespace = "",
@@ -82,7 +84,7 @@ final class CsvParser(
 		ParserRetVal.eitherToComplex(if (endState.innerInput.isEmpty) {
 			endState.value
 		} else {
-			endState.value.right.flatMap{x => builder.apply(x, endState.innerIndex, endState.innerInput, new LineParser)}
+			endState.value.right.flatMap{x => builder.apply(x, endState.innerIndex, endState.innerInput, lineParser)}
 		})
 	}
 	
@@ -94,76 +96,6 @@ final class CsvParser(
 	 * @return the parsed object
 	 */
 	def parse[A](builder:Builder[Int, String, A], chars:java.io.Reader):ParserRetVal[A,String] = this.parse(builder, new Reader2Iterable(chars))
-	
-	
-	/**
-	 * @param value the current value. Either a left describing an error, or a right with a complex value
-	 * @param innerIndex the current character index of the parse
-	 * @param innerInput the 
-	 * @param endingWhitespace Whitespace that should be ignored if a delimiter is encountered
-	 * 		before another non-whitespace character, but that should not be ignored otherwise.
-	 * @param quoted true if the parser is currently in a quoted state. characters between quote characters
-	 * 		are treated literally, so if this is true then characters should be treated literally,
-	 * 		and this should be toggled upon encountering a quote character.
-	 * @param escaped true if the parser is currently in an escape state. Characters after an escape character
-	 * 		is treated literally, so if this is true then characters should be treated literally,
-	 *		become true when an escape character is encountered and become false after encountering that literal character
-	 */
-	private[this] case class State[A] (
-		value:Either[(String,Int),A],
-		innerIndex:Int,
-		innerInput:String,
-		endingWhitespace:String,
-		quoted:Boolean,
-		escaped:Boolean
-	) {
-		def appendChar(c:Char):State[A] = this.copy(endingWhitespace = "", innerInput = innerInput + endingWhitespace + c)
-	}
-	
-	
-	/** Splits a CSV record (i.e. one line) into fields */
-	private[this] final class LineParser extends Parser[Int, String, String] {
-		def parse[A](builder:Builder[Int, String, A], chars:String):ParserRetVal[A,Nothing] = {
-			val endState = chars.zipWithIndex.foldLeft(State(Right(builder.init), 0, "", "", false, false)){(state, ci) => 
-				val (char, index) = ci
-				
-				if (state.escaped) {
-					state.appendChar(char).copy(escaped = false)
-				} else if (state.quoted) {
-					val isQuote = meaningfulCharacters.stringDelimeter contains char;
-					if (isQuote) {state.copy(quoted = false)}
-					else {state.appendChar(char)}
-				} else if (meaningfulCharacters.ignorable contains char) {
-					if (state.innerInput == "") {
-						state
-					} else {
-						state.copy(endingWhitespace = state.endingWhitespace + char)
-					}
-				} else if (meaningfulCharacters.escape contains char) {
-					state.copy(escaped = true)
-				} else if (meaningfulCharacters.stringDelimeter contains char) {
-					state.copy(quoted = true)
-				} else if (meaningfulCharacters.fieldDelimeter contains char) {
-					new State(
-						value = state.value.right.flatMap{x => builder.apply(x, state.innerIndex, state.innerInput, new IdentityParser[String]).left.map{x => ((x._1, x._2 + index))}},
-						innerIndex = state.innerIndex + 1,
-						innerInput = "",
-						endingWhitespace = "",
-						quoted = false,
-						escaped = false
-					)
-				} else {
-					state.appendChar(char)
-				}
-			}
-			
-			ParserRetVal.eitherToComplex(if (endState.innerInput.isEmpty) {
-				endState.value
-			} else {
-				endState.value.right.flatMap{x => builder.apply(x, endState.innerIndex, endState.innerInput, new IdentityParser[String])}
-			})
-		}
-	}
 }
 
 /**
@@ -213,4 +145,76 @@ object CsvParser {
 	 * @version 2.0
 	 */
 	val asciiCharacterMeanings = CharacterMeanings(Set('\u001E'), Set('\u001F'), Set.empty, Set.empty, Set.empty)
+	
+	
+	
+	
+	/**
+	 * @param value the current value. Either a left describing an error, or a right with a complex value
+	 * @param innerIndex the current character index of the parse
+	 * @param innerInput the 
+	 * @param endingWhitespace Whitespace that should be ignored if a delimiter is encountered
+	 * 		before another non-whitespace character, but that should not be ignored otherwise.
+	 * @param quoted true if the parser is currently in a quoted state. characters between quote characters
+	 * 		are treated literally, so if this is true then characters should be treated literally,
+	 * 		and this should be toggled upon encountering a quote character.
+	 * @param escaped true if the parser is currently in an escape state. Characters after an escape character
+	 * 		is treated literally, so if this is true then characters should be treated literally,
+	 *		become true when an escape character is encountered and become false after encountering that literal character
+	 */
+	private[parser] final case class State[A] (
+		value:Either[(String,Int),A],
+		innerIndex:Int,
+		innerInput:String,
+		endingWhitespace:String,
+		quoted:Boolean,
+		escaped:Boolean
+	) {
+		def appendChar(c:Char):State[A] = this.copy(endingWhitespace = "", innerInput = innerInput + endingWhitespace + c)
+	}
+	
+	
+	/** Splits a CSV record (i.e. one line) into fields */
+	private[parser] final class LineParser(meaningfulCharacters:CsvParser.CharacterMeanings) extends Parser[Int, String, String] {
+		def parse[A](builder:Builder[Int, String, A], chars:String):ParserRetVal[A,Nothing] = {
+			val endState = chars.zipWithIndex.foldLeft(State(Right(builder.init), 0, "", "", false, false)){(state, ci) =>
+				val (char, index) = ci
+				
+				if (state.escaped) {
+					state.appendChar(char).copy(escaped = false)
+				} else if (state.quoted) {
+					val isQuote = meaningfulCharacters.stringDelimeter contains char;
+					if (isQuote) {state.copy(quoted = false)}
+					else {state.appendChar(char)}
+				} else if (meaningfulCharacters.ignorable contains char) {
+					if (state.innerInput == "") {
+						state
+					} else {
+						state.copy(endingWhitespace = state.endingWhitespace + char)
+					}
+				} else if (meaningfulCharacters.escape contains char) {
+					state.copy(escaped = true)
+				} else if (meaningfulCharacters.stringDelimeter contains char) {
+					state.copy(quoted = true)
+				} else if (meaningfulCharacters.fieldDelimeter contains char) {
+					new State(
+						value = state.value.right.flatMap{x => builder.apply(x, state.innerIndex, state.innerInput, new IdentityParser[String]).left.map{x => ((x._1, x._2 + index))}},
+						innerIndex = state.innerIndex + 1,
+						innerInput = "",
+						endingWhitespace = "",
+						quoted = false,
+						escaped = false
+					)
+				} else {
+					state.appendChar(char)
+				}
+			}
+			
+			ParserRetVal.eitherToComplex(if (endState.innerInput.isEmpty) {
+				endState.value
+			} else {
+				endState.value.right.flatMap{x => builder.apply(x, endState.innerIndex, endState.innerInput, new IdentityParser[String])}
+			})
+		}
+	}
 }
--- src/main/scala/com/rayrobdod/json/parser/CsvWithHeaderParser.scala
+++ src/main/scala/com/rayrobdod/json/parser/CsvWithHeaderParser.scala
@@ -45,6 +45,7 @@ import com.rayrobdod.json.union.{StringOrInt, ParserRetVal}
 final class CsvWithHeaderParser(
 		meaningfulCharacters:CsvParser.CharacterMeanings = CsvParser.csvCharacterMeanings
 ) extends Parser[StringOrInt, String, Iterable[Char]] {
+	val lineParser = new CsvParser.LineParser(meaningfulCharacters)
 	
 	/**
 	 * Decodes the input values to an object.
@@ -68,10 +69,10 @@ final class CsvWithHeaderParser(
 				state.appendChar(char).copy(quoted = true)
 			} else if (meaningfulCharacters.recordDelimeter contains char) {
 				state.keys.fold{
-					val result = new CsvParser(meaningfulCharacters).parse(new SeqBuilder(new PrimitiveSeqBuilder[String]), state.innerInput)
-					result.fold({x:Seq[Seq[String]] =>
+					val result = lineParser.parse(new PrimitiveSeqBuilder[String], state.innerInput)
+					result.fold({x:Seq[String] =>
 						new State[A](
-							keys = Option(x.flatten),
+							keys = Option(x),
 							value = state.value,
 							innerIndex = state.innerIndex + 1,
 							innerInput = "",
@@ -94,7 +95,11 @@ final class CsvWithHeaderParser(
 				}{keys:Seq[String] =>
 					new State[A](
 						keys = state.keys,
-						value = state.value.right.flatMap{x => builder.apply(x, state.innerIndex, state.innerInput, new LineParser(keys)).left.map{x => ((x._1, x._2 + index))}},
+						value = state.value.right.flatMap{x =>
+							builder.apply(x, state.innerIndex, state.innerInput, lineParser.mapKey[StringOrInt]{i =>
+								if (keys.isDefinedAt(i)) {StringOrInt(keys(i))} else {StringOrInt(i)}
+							}).left.map{x => ((x._1, x._2 + index))}
+						},
 						innerIndex = state.innerIndex + 1,
 						innerInput = "",
 						endingWhitespace = "",
@@ -113,7 +118,11 @@ final class CsvWithHeaderParser(
 			endState.keys.fold{
 				endState.value
 			}{keys =>
-				endState.value.right.flatMap{x => builder.apply(x, endState.innerIndex, endState.innerInput, new LineParser(keys))}
+				endState.value.right.flatMap{x => 
+					builder.apply(x, endState.innerIndex, endState.innerInput, lineParser.mapKey[StringOrInt]{i =>
+						if (keys.isDefinedAt(i)) {StringOrInt(keys(i))} else {StringOrInt(i)}
+					})
+				}
 			}
 		}).fold({case (msg,idx) => ParserRetVal.Failure(msg,idx)}, {x => ParserRetVal.Complex(x)})
 	}
@@ -128,7 +137,7 @@ final class CsvWithHeaderParser(
 	def parse[A](builder:Builder[StringOrInt, String, A], chars:java.io.Reader):ParserRetVal[A,String] = this.parse(builder, new Reader2Iterable(chars))
 	
 	
-	private[this] case class State[A] (
+	private[this] final case class State[A] (
 		keys:Option[Seq[String]],
 		value:Either[(String,Int),A],
 		innerIndex:Int,
@@ -138,55 +147,5 @@ final class CsvWithHeaderParser(
 		escaped:Boolean
 	) {
 		def appendChar(c:Char):State[A] = this.copy(endingWhitespace = "", innerInput = innerInput + endingWhitespace + c)
-	}
-
-	
-	/** Splits a CSV record (i.e. one line) into fields */
-	private[this] final class LineParser(keys:Seq[String]) extends Parser[StringOrInt, String, String] {
-		def parse[A](builder:Builder[StringOrInt, String, A], chars:String):ParserRetVal[A,Nothing] = {
-			val endState = chars.zipWithIndex.foldLeft(State(Option(keys), Right(builder.init), 0, "", "", false, false)){(state, ci) => 
-				val (char, index) = ci
-				
-				if (state.escaped) {
-					state.appendChar(char).copy(escaped = false)
-				} else if (state.quoted) {
-					val isQuote = meaningfulCharacters.stringDelimeter contains char;
-					if (isQuote) {state.copy(quoted = false)}
-					else {state.appendChar(char)}
-				} else if (meaningfulCharacters.ignorable contains char) {
-					if (state.innerInput == "") {
-						state
-					} else {
-						state.copy(endingWhitespace = state.endingWhitespace + char)
-					}
-				} else if (meaningfulCharacters.escape contains char) {
-					state.copy(escaped = true)
-				} else if (meaningfulCharacters.stringDelimeter contains char) {
-					state.copy(quoted = true)
-				} else if (meaningfulCharacters.fieldDelimeter contains char) {
-					new State(
-						keys = state.keys,
-						value = state.value.right.flatMap{x => builder.apply(x, keys(state.innerIndex), state.innerInput, new IdentityParser[String]).left.map{x => ((x._1, x._2 + index))}},
-						innerIndex = state.innerIndex + 1,
-						innerInput = "",
-						endingWhitespace = "",
-						quoted = false,
-						escaped = false
-					)
-				} else {
-					state.appendChar(char)
-				}
-			}
-			
-			(if (endState.innerInput.isEmpty) {
-				endState.value
-			} else {
-				endState.keys.fold{
-					endState.value
-				}{keys =>
-					endState.value.right.flatMap{x => builder.apply(x, keys(endState.innerIndex), endState.innerInput, new IdentityParser[String])}
-				}
-			}).fold({case (msg,idx) => ParserRetVal.Failure(msg,idx)}, {x => ParserRetVal.Complex(x)})
-		}
 	}
 }
